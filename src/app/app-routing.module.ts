d6e8f3ead3dbd1e992cc1ba5fd3ad4e61dbb0442
Rico Apon <rico.apon@hotmail.com>
2020-06-28 15:39:55 +0200

    Add Font Awesome icons left of menu items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,14 +10,14 @@ import {EnglishComponent} from './english/english.component';
 
 /* This element is used to dynamically generate the links in the navigation bar. */
 export const navRoutes: Routes = [
-  { path: 'collega', component: CollegaComponent, data: {name: 'Collega', screenOrder: 5} },
-  { path: 'contact', component: ContactComponent, data: {name: 'Contact', screenOrder: 7} },
-  { path: 'cursist', component: CursistComponent, data: {name: 'Cursist', screenOrder: 3} },
-  { path: 'english', component: EnglishComponent, data: {name: 'English', screenOrder: 6} },
-  { path: 'voorstellen', component: VoorstellenComponent, data: {name: 'Voorstellen', screenOrder: 2} },
-  { path: 'werkgever', component: WerkgeverComponent, data: {name: 'Werkgever', screenOrder: 4} },
+  { path: 'collega', component: CollegaComponent, data: {name: 'Collega', screenOrder: 5, iconClass: 'fas fa-people-arrows'} },
+  { path: 'contact', component: ContactComponent, data: {name: 'Contact', screenOrder: 7, iconClass: 'far fa-address-card'} },
+  { path: 'cursist', component: CursistComponent, data: {name: 'Cursist', screenOrder: 3, iconClass: 'fas fa-graduation-cap'} },
+  { path: 'english', component: EnglishComponent, data: {name: 'English', screenOrder: 6, iconClass: 'fas fa-globe-americas'} },
+  { path: 'voorstellen', component: VoorstellenComponent, data: {name: 'Voorstellen', screenOrder: 2, iconClass: 'fas fa-user'} },
+  { path: 'werkgever', component: WerkgeverComponent, data: {name: 'Werkgever', screenOrder: 4, iconClass: 'fas fa-briefcase'} },
 
-  { path: '', component: HomeComponent, data: {name: "Home", screenOrder: 1} },
+  { path: '', component: HomeComponent, data: {name: "Home", screenOrder: 1, iconClass: 'fas fa-home'} },
 ]
 
 const otherRoutes: Routes = [
