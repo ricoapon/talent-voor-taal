261cb126477452d25a14f12a8147788fc30e495c
Rico Apon <rico.apon@gmail.com>
2021-08-05 18:36:24 +0200

    Rewrite routing in logical order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,12 @@ import {EnglishComponent} from './english/english.component';
 
 /* This element is used to dynamically generate the links in the navigation bar. */
 export const navRoutes: Routes = [
-  { path: 'collega', component: CollegaComponent, data: {name: 'Collega', screenOrder: 5, iconClass: 'fas fa-people-arrows'} },
-  { path: 'contact', component: ContactComponent, data: {name: 'Contact', screenOrder: 7, iconClass: 'far fa-address-card'} },
-  { path: 'cursist', component: CursistComponent, data: {name: 'Cursist', screenOrder: 3, iconClass: 'fas fa-graduation-cap'} },
-  { path: 'english', component: EnglishComponent, data: {name: 'English', screenOrder: 6, iconClass: 'fas fa-globe-americas'} },
   { path: 'voorstellen', component: VoorstellenComponent, data: {name: 'Voorstellen', screenOrder: 2, iconClass: 'fas fa-user'} },
-
+  { path: 'cursist', component: CursistComponent, data: {name: 'Cursist', screenOrder: 3, iconClass: 'fas fa-graduation-cap'} },
+  { path: 'collega', component: CollegaComponent, data: {name: 'Collega', screenOrder: 4, iconClass: 'fas fa-people-arrows'} },
+  { path: 'english', component: EnglishComponent, data: {name: 'English', screenOrder: 5, iconClass: 'fas fa-globe-americas'} },
+  { path: 'contact', component: ContactComponent, data: {name: 'Contact', screenOrder: 6, iconClass: 'far fa-address-card'} },
+  // Empty path must be last, since this will always match.
   { path: '', component: HomeComponent, data: {name: "Home", screenOrder: 1, iconClass: 'fas fa-home'} },
 ]
 
